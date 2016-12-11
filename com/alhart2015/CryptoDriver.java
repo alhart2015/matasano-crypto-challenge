dd095dcd96e0dfb2c67afa3930bb7ee5fb687652
Alden Hart <alden.hart9@gmail.com>
2016-12-11 17:30:49 -0500

    Add a skeleton for set 1 problem 1. Still need to properly construct the directory structure n stuff.

---
+++ com/alhart2015/CryptoDriver.java
@@ -0,0 +1,15 @@
+package com.alhart2015;
+
+import sets.one.HexToSixtyFourConverter;
+
+/*
+Driver program for the crypto challenge.
+*/
+class CryptoDriver {
+
+    public static void main(String[] args) {
+        System.out.println("Getting started");
+
+        HexToSixtyFourConverter.convert();
+    }
+}
---
+++ com/alhart2015/sets/one/HexToSixtyFourConverter.java
@@ -0,0 +1,26 @@
+package com.alhart2015.sets.one;
+
+/*
+Set 1 Challenge 1:
+
+Convert hex to base64
+The string:
+
+49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+Should produce:
+
+SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+So go ahead and make that happen. You'll need to use this code for 
+the rest of the exercises.
+
+Cryptopals Rule
+Always operate on raw bytes, never on encoded strings. Only use hex 
+and base64 for pretty-printing.
+*/
+
+class HexToSixtyFourConverter {
+
+    public static byte[] convert() {
+        System.out.println("The import worked");
+    }
+}
